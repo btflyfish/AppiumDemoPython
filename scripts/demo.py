607acdfc9a5a1d206e58bb2eaf905d9b4dc3f897
btflyfish <42028269+btflyfish@users.noreply.github.com>
2018-08-03 10:34:25 +0800

    Update demo.py
    修改Demo.py文件

--- scripts/demo.py
+++ scripts/demo.py
@@ -2,11 +2,11 @@ from appium import webdriver
 import time
 #server启动参数
 desired_caps={}
-#设备信息
+#设备信息安卓手机型号
 desired_caps['platformName']='Android'  #设备版本号
 desired_caps['platformVersion']='5.1'  #手机系统版本号
 desired_caps['deviceName']='192.168.57.103:5555'  #手机或者模拟器类型
-#app信息，得到包名和启动名
+#app信息，得到包名和启动名，请查阅文章
 # 参阅文章https://www.cnblogs.com/unknows/p/7736965.html
 desired_caps['appPackage']='com.android.settings'
 desired_caps['appActivity']='.Settings'
